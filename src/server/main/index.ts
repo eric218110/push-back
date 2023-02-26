113c8a5f95575337e7a5f3f6d6d39f3fa5270838
Eric Silva <ericsilvaccp@gmail.com>
2023-02-26 11:08:53 -0300

    feat: cofig cors

--- src/server/main/index.ts
+++ src/server/main/index.ts
@@ -1,10 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './../../shared/app/app.module';
+import { HttpExceptionFilter } from './../../shared/filter/http/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors({
+    origin: '*',
+    methods: '*'
+  })
   await app.listen(3000);
 }
 bootstrap();
