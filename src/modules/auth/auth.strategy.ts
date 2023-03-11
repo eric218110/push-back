ae7d61657295172979a7032f74e3ff5d09095c84
Eric Silva <ericsilvaccp@gmail.com>
2023-03-11 15:22:39 -0300

    feat: handler error

--- src/modules/auth/auth.strategy.ts
+++ src/modules/auth/auth.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UserService } from './../user/user.service';
@@ -19,12 +19,12 @@ export class AuthStrategy extends PassportStrategy(Strategy) {
   async validate({ iat, exp, userId }: AuthWithIatAndEat, next: Function) {
     const timeDiff = exp - iat;
     if (timeDiff <= 0) {
-      throw new UnauthorizedException();
+      throw new HttpException('Token expired or invalid - token', HttpStatus.UNAUTHORIZED);
     }
 
-    const user = await this.usersService.readUserById(userId);
+    const user = await this.usersService.readUserById(userId).catch(() => undefined);
 
-    if (!user) throw new UnauthorizedException();
+    if (!user) throw new HttpException('Token expired or invalid - token', HttpStatus.UNAUTHORIZED);
 
     next(null, user);
   }
